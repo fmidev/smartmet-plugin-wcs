7e6db53d165a15e9fe6313de35c0566ceb3bfe7e
pjukka <pjukka@users.noreply.github.com>
2017-09-21 14:01:10 +0300

    Separate Element class from DimensionSubset.

---
+++ include/Element.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <spine/Value.h>
+#include <xercesc/dom/DOMDocument.hpp>
+
+namespace SmartMet
+{
+namespace Plugin
+{
+namespace WCS
+{
+class Element
+{
+ public:
+  using NameType = std::string;
+  using Value = Spine::Value;
+  Element(const xercesc::DOMElement* element);
+  Element(const Value& value, const NameType& name);
+  virtual ~Element() {}
+  const Value& getValue() const { return mValue; }
+  const NameType& getName() const { return mName; }
+
+ private:
+  Value mValue;
+  NameType mName;
+};
+}
+}
+}
