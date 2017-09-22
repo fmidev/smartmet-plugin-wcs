884a79f3ae5205384454bda4448715699956c24d
pjukka <pjukka@users.noreply.github.com>
2017-09-22 15:45:37 +0300

    Throw an exception if CRS appreviation is empty.
    Appreviation is needed in axisLabels of response of DescribeCoverage
    request. Dataset names are also used when a dataset is trimmed and sliced.

--- source/CompoundCRS.cpp
+++ source/CompoundCRS.cpp
@@ -25,6 +25,12 @@ void CRSBase::parse(boost::shared_ptr<SmartMet::Spine::ConfigBase> config,
       config->get_mandatory_config_param<CRSBase::Identifier>(setting, "identifier");
 
   CRSBase::Abbrev abbrev = config->get_mandatory_config_param<CRSBase::Abbrev>(setting, "abbrev");
+  if (abbrev.empty())
+  {
+    std::ostringstream msg;
+    msg << "Abbreviation (abbrev) of '" << identifier << "' CRS configuration cannot be empty.";
+    throw std::runtime_error(msg.str());
+  }
 
   CRSBase::setIdentifier(identifier);
   CRSBase::setAbbrev(abbrev);
