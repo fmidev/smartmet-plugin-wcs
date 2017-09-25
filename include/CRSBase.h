6f6c367323de13bc5bb34299a4a30adb2ddab3aa
pjukka <pjukka@users.noreply.github.com>
2017-09-25 12:48:27 +0300

    Create separate files for the classes in CoumpoundCRS file.

---
+++ include/CRSBase.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <spine/ConfigBase.h>
+
+namespace SmartMet
+{
+namespace Plugin
+{
+namespace WCS
+{
+class CRSBase
+{
+ public:
+  using Identifier = std::string;
+  using Abbrev = std::string;
+  CRSBase() {}
+  virtual ~CRSBase() {}
+  inline const Abbrev& getAbbrev() const { return mAbbrev; }
+  inline const Identifier& getIdentifier() const { return mIdentifier; }
+
+ protected:
+  void parse(boost::shared_ptr<SmartMet::Spine::ConfigBase> config, libconfig::Setting& setting);
+  void setAbbrev(const Abbrev& abbrev);
+  void setIdentifier(const Identifier& identifier);
+
+ private:
+  Identifier mIdentifier;
+  Abbrev mAbbrev;
+};
+}
+}
+}
